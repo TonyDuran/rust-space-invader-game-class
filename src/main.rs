ddd754c034b8494227cd638f5ddd8d3ad638a0cc
Tony Duran <tony-duran@outlook.com>
2024-04-01 20:32:31 -0500

    getting music to play

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@ use rusty_audio::Audio;
 
 fn main() -> Result <(), Box<dyn Error>>{
     let mut audio = Audio::new();
-    audio.add("explode", "../explode.wav");
-    // audio.add("lose", "lose.wav");
-    // audio.add("move", "move.wav");
-    // audio.add("pew", "new.wav");
-    // audio.add("startup", "startup.wav");
-    // audio.add("win", "win.wav");
-    // audio.play("startup");
+    for item in &["explode", "lose", "move", "pew", "startup", "win"] {
+        audio.add(item, &format!("audio/{}.wav", item));
+    }
+    audio.play("startup");
 
     // Cleanup
     audio.wait();
